fa3753b0787421a54b9f15decedab21b097ee5a5
Gaurav <gauravp@zignuts.com>
2023-04-28 19:37:12 +0530

    api

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,15 @@
 <?php
 
 use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\ItemController;
+use App\Http\Controllers\PaymentController;
 use App\Http\Controllers\PlanController;
+use App\Http\Controllers\PriceController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Stripe\Price;
+use Stripe\Product;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,26 +27,43 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 
-//Customer :
 
-Route::controller(CustomerController::class)->prefix('customer')->group(function () {
-    Route::post('create', 'create');
-    Route::post('update/{customer_id}', 'update');
-    Route::post('delete/{customer_id}', 'delete');
-});
-
-// Product :
-
+// Product
 Route::controller(ProductController::class)->prefix('product')->group(function () {
     Route::post('create', 'create');
     Route::post('update/{product_id}', 'update');
     Route::post('delete/{product_id}', 'delete');
 });
 
-// Plan :
-
-Route::controller(PlanController::class)->prefix('plan')->group(function(){
+// Plan
+Route::controller(PlanController::class)->prefix('plan')->group(function () {
     Route::post('create', 'create');
-    Route::post('update/{plan_id}', 'update');
-    Route::post('delete/{plan_id}', 'delete');
+    Route::post('update/{product_id}', 'update');
+    Route::post('delete/{product_id}', 'delete');
+});
+
+// Customer
+Route::controller(CustomerController::class)->prefix('customer')->group(function () {
+    Route::post('create', 'create');
+    Route::post('update/{customer_id}', 'update');
+    Route::post('delete/{customer_id}', 'delete');
+});
+
+// Item
+Route::controller(ItemController::class)->prefix('item')->group(function () {
+    Route::post('create', 'create');
+    Route::post('update/{item_id}', 'update');
+    Route::post('delete/{item_id}', 'delete');
+});
+
+// Price
+Route::controller(PriceController::class)->prefix('price')->group(function () {
+    Route::post('create', 'create');
+    Route::post('update/{product_id}', 'update');
+    Route::post('delete/{product_id}', 'delete');
+});
+
+Route::controller(PaymentController::class)->prefix('payment')->group(function () {
+    Route::post('add-card', 'addCard');
+    Route::post('create-intent','createIntent');
 });
