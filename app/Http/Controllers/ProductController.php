9609653d0b4fc294b1a21692578ce560c1eecd43
Gaurav <gauravp@zignuts.com>
2023-05-01 19:10:02 +0530

    create update delete

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Product;
+use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
+use Stripe\Exception\ApiErrorException;
 use Stripe\Product as StripeProduct;
 
 class ProductController extends Controller
@@ -20,23 +21,24 @@ class ProductController extends Controller
         if ($validation->fails())
             return error('Validation Error', $validation->errors(), 'validation');
 
-        $stripeProduct = StripeProduct::create([
-            'name'          => $request->name,
-            'description'   => $request->description,
-            'images'        => [$request->image]
-        ]);
+        try {
+            $stripeProduct = StripeProduct::create([
+                'name'          => $request->name,
+                'description'   => $request->description,
+                'images'        => [$request->image]
+            ]);
 
-        $product = Product::create($request->only(['name', 'description', 'image']) + [
-            'stripe_product_id' => $stripeProduct->id
-        ]);
-
-        return ok('Product Created', $product);
+            return ok('Product Created', $stripeProduct);
+        } catch (ApiErrorException $ae) {
+            return error('Strpe API Error', $ae->getMessage());
+        } catch (Exception $e) {
+            return error('Error', $e->getMessage());
+        }
     }
 
 
-    public function update($productId, Request $request)
+    public function update($stripe_product_id, Request $request)
     {
-        $product = Product::where('stripe_product_id', $productId)->first();
 
         $validation = Validator::make($request->all(), [
             'name'          => 'required|string',
@@ -46,24 +48,31 @@ class ProductController extends Controller
 
         if ($validation->fails())
             return error('Validation Error', $validation->errors(), 'validation');
+        try {
+            StripeProduct::update($stripe_product_id, [
+                'name'          => $request->name,
+                'description'   => $request->description,
+                'images'        => [$request->image]
+            ]);
 
-        StripeProduct::update($product->stripe_product_id, [
-            'name'          => $request->name,
-            'description'   => $request->description,
-            'images'        => [$request->image]
-        ]);
-
-        $product->update($request->only(['name', 'description', 'image']));
-
-        return ok('Product Updated Successfull.');
+            return ok('Product Updated Successfull.');
+        } catch (ApiErrorException $ae) {
+            return error('Strpe API Error', $ae->getMessage());
+        } catch (Exception $e) {
+            return error('Error', $e->getMessage());
+        }
     }
 
-    public function delete($productId, Request $request)
+    public function delete($stripe_product_id)
     {
-        $product = Product::where('stripe_product_id', $productId)->first();
-        $stripeProduct = StripeProduct::retrieve($product->stripe_product_id);
-        $stripeProduct->delete();
-        $product->delete();
-        return ok('Product Deleted');
+        try {
+            $stripeProduct = StripeProduct::retrieve($stripe_product_id);
+            $stripeProduct->delete();
+            return ok('Product Deleted');
+        } catch (ApiErrorException $ae) {
+            return error('Strpe API Error', $ae->getMessage());
+        } catch (Exception $e) {
+            return error('Error', $e->getMessage());
+        }
     }
 }
