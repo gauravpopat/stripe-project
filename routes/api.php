e647dd3bf3bebee57bbd64f8eaa3592e9360d56b
Gaurav <gauravp@zignuts.com>
2023-05-01 19:08:28 +0530

    api

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,12 @@
 <?php
 
 use App\Http\Controllers\CustomerController;
-use App\Http\Controllers\ItemController;
+use App\Http\Controllers\InvoiceController;
 use App\Http\Controllers\PaymentController;
 use App\Http\Controllers\PlanController;
 use App\Http\Controllers\PriceController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\SubscriptionController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Stripe\Price;
@@ -49,21 +50,23 @@ Route::controller(CustomerController::class)->prefix('customer')->group(function
     Route::post('delete/{customer_id}', 'delete');
 });
 
-// Item
-Route::controller(ItemController::class)->prefix('item')->group(function () {
-    Route::post('create', 'create');
-    Route::post('update/{item_id}', 'update');
-    Route::post('delete/{item_id}', 'delete');
-});
-
 // Price
 Route::controller(PriceController::class)->prefix('price')->group(function () {
     Route::post('create', 'create');
-    Route::post('update/{product_id}', 'update');
-    Route::post('delete/{product_id}', 'delete');
 });
 
 Route::controller(PaymentController::class)->prefix('payment')->group(function () {
     Route::post('add-card', 'addCard');
-    Route::post('create-intent','createIntent');
+    Route::post('create-intent', 'createIntent');
+    Route::post('checkout-session', 'createCheckoutSession');
+});
+
+Route::controller(SubscriptionController::class)->prefix('subscription')->group(function () {
+    Route::post('create', 'create');
+    Route::post('update/{subscription_id}', 'update');
+});
+
+Route::controller(InvoiceController::class)->prefix('invoice')->group(function () {
+    Route::post('create', 'create');
+    Route::get('get-invoice/{invoice_id}', 'getInvoice');
 });
